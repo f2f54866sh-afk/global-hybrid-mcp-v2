af0b3d1aa6a312022b658ae9699e3522e5b0129c
f2f54866sh-afk <f2f54866sh@privaterelay.appleid.com>
2026-09-04 16:33:56 +0800

    Persist RC-01 locus regressions

--- tests/test_failure_locus.py
+++ tests/test_failure_locus.py
@@ -1,5 +1,10 @@
 from global_hybrid_v2.contracts import FailureLocus
-from global_hybrid_v2.governance.failure_locus import classify_failure_locus, may_reopen_existing, may_repair
+from global_hybrid_v2.governance.failure_locus import (
+    classify_failure_locus,
+    may_reassess_platform,
+    may_reopen_existing,
+    may_repair,
+)
 
 
 def test_host_recurrence_is_parked_without_repair():
@@ -18,4 +23,18 @@ def test_owned_matching_regression_can_reopen():
 def test_unknown_locus_is_evidence_only():
     locus = classify_failure_locus(entered_user_controlled_runtime=False)
     assert locus is FailureLocus.UNKNOWN_LOCUS
+    assert not may_repair(locus=locus)
+
+
+def test_contradictory_runtime_and_platform_evidence_is_unknown():
+    assert classify_failure_locus(
+        entered_user_controlled_runtime=True, platform_bypass=True
+    ) is FailureLocus.UNKNOWN_LOCUS
+
+
+def test_platform_reassessment_does_not_authorize_repair():
+    locus = FailureLocus.HOST_PLATFORM
+    assert may_reassess_platform(
+        locus=locus, fresh_platform_capability_change_evidence=True
+    )
     assert not may_repair(locus=locus)
